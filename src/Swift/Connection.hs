674d2ae8d52f489e3fc7a1fb2e66fa2a24656a15
Maxim Mitroshin <rocco66max@gmail.com>
2013-06-30 19:15:15 +0400

    Use HTTP instad of http-conduit, first compile

--- src/Swift/Connection.hs
+++ src/Swift/Connection.hs
@@ -9,11 +9,10 @@
 module Swift.Connection
     ( SwiftAuthenticator(..)
     , SwiftAuthUrl
-    , addHeader
+    , test
     ) where
 
 import Data.Functor ((<$>))
-import Data.Conduit (ResourceT, MonadResource)
 import Data.ByteString (ByteString)
 import Data.Typeable (Typeable)
 import Data.Maybe (fromJust)
@@ -22,24 +21,36 @@ import Control.Monad.IO.Class (MonadIO)
 import Control.Monad.Base (MonadBase)
 import Control.Monad.Reader (ReaderT)
 import Control.Monad.Reader.Class (MonadReader(ask))
+import Control.Monad.Reader (runReaderT)
 import Control.Monad.State.Lazy (StateT(runStateT), MonadState(put, get))
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad (void)
 import qualified Data.List as List
 
+import Control.Monad.Catch (CatchT, MonadCatch(throwM), Exception, runCatchT)
 import Data.CaseInsensitive (CI)
-import Data.Conduit (MonadResource, MonadBaseControl)
-import Network.HTTP.Conduit (Request(requestHeaders), Response, parseUrl,
-                             withManager, httpLbs, responseHeaders)
-import Network.HTTP.Types.Header (ResponseHeaders, HeaderName)
+import Network.Http.Client (Request, Response, RequestBuilder, Method(GET),
+                            getHeader, http, receiveResponse,
+                            setHeader, sendRequest, emptyBody, openConnection,
+                            buildRequest)
+import Data.String.Like (string)
+import OpenSSL (withOpenSSL)
 
 type SwiftAuthUrl = String
-type Url = String
+type Url = ByteString
 type SwiftAuthToken = ByteString
+
+data SwiftException = NoStorageUrlSwiftException
+                    | NoStorageTokenSwiftException
+     deriving (Show, Typeable)
+
+instance Exception SwiftException
 
 -- MonadIO m
 class SwiftAuthenticator a where
-    addRequestAuthInfo :: a -> Request Swift -> Request Swift
-    prepareRequest :: a -> Response body -> Request Swift -> Request Swift
-    prepareRequest auth response request = request
+    addRequestAuthInfo :: a -> RequestBuilder ()
+    prepareRequest :: a -> Response -> RequestBuilder ()
+    prepareRequest auth response = return ()
 
 data SelcdnAuth = SelcdnAuth
     { selcdnAuthAccount :: {-# UNPACK #-} !ByteString
@@ -48,59 +59,48 @@ data SelcdnAuth = SelcdnAuth
     } deriving (Show, Eq, Typeable)
 
 instance SwiftAuthenticator SelcdnAuth where
-    addRequestAuthInfo SelcdnAuth { .. } =
-        addHeader "X-Auth-User" selcdnAuthAccount .
-        (addHeader "X-Auth-Key" selcdnAuthKey)
+    addRequestAuthInfo SelcdnAuth { .. } = do
+        setHeader "X-Auth-User" selcdnAuthAccount
+        setHeader "X-Auth-Key" selcdnAuthKey
 
 -- TODO: add conduit manager to state
 data SwiftConnectInfo = SwiftConnectInfo
     { swiftConnectInfoUrl :: {-# UNPACK #-} !Url
     , swiftConnectToken   :: {-# UNPACK #-} !SwiftAuthToken }
 
-newtype Swift a = Swift { unSwift :: StateT SwiftConnectInfo
-                                         (ReaderT SelcdnAuth
-                                             (ResourceT IO)) a }
-    deriving (Monad, Functor, Applicative, MonadBase IO, MonadReader SelcdnAuth,
-              MonadState SwiftConnectInfo)
-
-
-addHeader :: CI ByteString -> ByteString -> Request Swift -> Request Swift
-addHeader name val req = req { requestHeaders = newHeaders }
-  where
-    newHeaders = requestHeaders req ++ [(name, val)]
-
--- withSwift :: (SwiftAuthenticator a, MonadIO m)
---           => SwiftAuthUrl
---           -> a
---           -> Swift ()
---           -> m ()
--- withSwift authUrl authInfo action = do
---     authRequest <- addRequestAuthInfo authInfo <$> parseUrl authUrl
---     withManager $ \manager -> do
---         -- authResponse <- httpLbs authRequest manager
---         action
+newtype Swift a = Swift { unSwift :: ReaderT SelcdnAuth
+                                         (StateT SwiftConnectInfo
+                                             (CatchT IO)) a }
+    deriving (Monad, Functor, Applicative, MonadReader SelcdnAuth,
+              MonadState SwiftConnectInfo, MonadIO)
 
 makeAuthentification :: Swift ()
-makeAuthentification req = do
-    authInfo@SelcdnAuth { .. } <- ask
-    authRequest <- addRequestAuthInfo authInfo <$> parseUrl selcdnAuthUrl
-    withManager $ \manager -> do
-        -- authResponse <- httpLbs authRequest manager
-        headers <- responseHeaders <$> httpLbs authRequest manager
-        let url = findHeader "X-Auth-Url" headers
-            token = findHeader "X-Auth-Token" headers
-        put SwiftConnectInfo { swiftConnectInfoUrl = url
-                             , swiftConnectToken = token }
-  where
-    findHeader :: HeaderName -> ResponseHeaders -> ByteString
-    findHeader name = fromJust . List.find (\(n, _) -> n == name)
+makeAuthentification = do
+    authInfo <- ask
+    con <- liftIO $ openConnection "selcdn.ru" 443
+    authRequest <- liftIO $ buildRequest $ do
+         http GET "/redefine/me"
+         addRequestAuthInfo authInfo
 
-runSwift :: (SwiftAuthenticator a, MonadIO m)
-         => SwiftAuthUrl
-         -> a
-         -> Swift ()
-         -> m ()
-runSwift authUrl authInfo action = do
-    withManager $ \manager -> do
-        -- authResponse <- httpLbs authRequest manager
-        action
+    liftIO $ sendRequest con authRequest emptyBody
+    conInfo <- liftIO $ receiveResponse con $ \resp _is -> do
+        url <- getHeaderExc NoStorageUrlSwiftException resp "X-Auth-Url"
+        token <- getHeaderExc NoStorageTokenSwiftException resp "X-Auth-Token"
+        return SwiftConnectInfo { swiftConnectInfoUrl = url
+                                , swiftConnectToken   = token }
+    put conInfo
+  where
+    getHeaderExc :: (Exception e) => e -> Response -> ByteString -> IO ByteString
+    getHeaderExc exc resp name = maybe (throwM exc) return $ getHeader resp name
+
+runSwift :: SelcdnAuth -> Swift () -> IO ()
+runSwift authInfo action = let
+    initConInfo = SwiftConnectInfo "" ""  in
+    void $ runCatchT (runStateT (runReaderT (unSwift (makeAuthentification >> action)) authInfo ) initConInfo )
+
+test :: IO ()
+test = withOpenSSL $ let
+    authentificator = SelcdnAuth { selcdnAuthAccount = "7091_hswift"
+                                 , selcdnAuthKey = "bh18Px6zxg"
+                                 , selcdnAuthUrl = "auth.selcdn.ru" } in
+    runSwift authentificator (return ())
